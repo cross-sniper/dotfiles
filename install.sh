c32eff1b0960a4615b9456981bcc665e46bda501
cross-sniper <cts.contact.br@gmail.com>
2024-05-21 08:00:35 -0300

    added a warning for alacritty and tmux

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 function installConf(){
     file_origin=$(pwd)/$1
     destination=~/$1
@@ -14,12 +14,20 @@ function installConf(){
     fi
 }
 
-installConf .zshrc
-installConf .alacritty.toml
+installConf .zshrc # zsh config
+installConf .alacritty.toml # alacritty config
 if [ ! -d ~/.tmux/plugins/tpm ]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 else
     echo "tmux plugin manager allready installed"
 fi
-installConf .tmux.conf
-installConf .config/nvim
+
+installConf .tmux.conf # tmux config
+installConf .config/nvim # neovim config
+
+# themes for alacritty(the one in the config is tokyo night, "/usr/lib/node_modules/alacritty-themes/themes/Tokyonight_Night.toml")
+if [ ! -d "/usr/lib/node_modules/alacritty-themes/" ]; then
+    echo "you might need to install \"alacritty-themes\""
+    echo "opening https://github.com/rajasegar/alacritty-themes"
+    xdg-open "https://github.com/rajasegar/alacritty-themes"
+fi
