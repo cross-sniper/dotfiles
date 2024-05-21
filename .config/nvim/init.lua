efc7642e1c06af97bf9936fa5a241f9e3519c084
cross-sniper <cts.contact.br@gmail.com>
2024-05-21 08:57:48 -0300

    vim config kinda done

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,23 @@
 -- nvim init.lua
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+require("keymaps")
+require("plugins")
+
+tokyonight_style = 'night'
+vim.colorscheme = 'tokyonight'
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,24 @@
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+-- Using a non-default branch
+Plug('neoclide/coc.nvim', { ['branch'] = 'release' , ['do']="npm ci" })
+
+-- Use 'dir' option to install plugin in a non-default directory
+Plug('junegunn/fzf', { ['dir'] = '~/.fzf' })
+
+-- Post-update hook: run a shell command after installing or updating the plugin
+Plug('junegunn/fzf', { ['dir'] = '~/.fzf', ['do'] = './install --all' })
+Plug('jiangmiao/auto-pairs')
+
+-- Post-update hook can be a lambda expression
+Plug('junegunn/fzf', { ['do'] = function()
+  vim.fn['fzf#install']()
+end })
+
+-- On-demand loading: loaded when the specified command is executed
+Plug('preservim/nerdtree', { ['on'] = 'NERDTreeToggle' })
+Plug('ghifarit53/tokyonight-vim')
+vim.call('plug#end')
